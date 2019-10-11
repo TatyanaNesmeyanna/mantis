194d10aa3df56228c8b84e395ea448746817ce6c
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-10-11 21:17:39 +0300

    login test

---
+++ fixture/application.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from selenium.common.exceptions import NoSuchElementException
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser=="firefox":
+            self.wd = webdriver.Firefox()
+        elif browser=="Chrome":
+            self.wd = webdriver.Chrome()
+        elif browser=="ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("/addressbook/")):
+            wd.get(self.base_url)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def is_login_page(self):
+        try:
+            self.wd.find_element_by_name("pass")
+            return True
+        except NoSuchElementException:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
