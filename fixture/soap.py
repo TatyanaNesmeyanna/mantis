2791b8ba747bea27ed2be6c04b870865d9e75524
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-10-17 15:17:03 +0300

    getting baseUrl from target.json on soap.py

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,14 +1,15 @@
 from suds.client import Client
 from suds import WebFault
 from model.project import Project
-
+import re
 
 class SoapHelper:
     def __init__(self, app):
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        # client = Client("http://localhost/mantisbt-1.2.20/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client("".join([self.app.config['web']['baseUrl'], "api/soap/mantisconnect.php?wsdl"]))
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +18,8 @@ class SoapHelper:
 
 
     def get_projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        # client = Client("http://localhost/mantisbt-1.2.20/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client("".join([self.app.config['web']['baseUrl'], "api/soap/mantisconnect.php?wsdl"]))
         try:
             projects_list = [Project(pr.name) for pr in client.service.mc_projects_get_user_accessible(username, password)]
             return projects_list
